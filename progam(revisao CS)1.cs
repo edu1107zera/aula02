cc4010af7368b6d7943db91bfa349de566727441
edu1107zera <128429561+edu1107zera@users.noreply.github.com>
2023-06-07 14:49:02 -0300

    Add files via upload

---
+++ progam(revisao CS)1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edu02textos
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string nome = "Carol";
+            int idade = 19;
+            Console.WriteLine("progama criado por " + nome);
+            Console.WriteLine("lembrando que foi criado por " + nome);
+
+            Console.WriteLine(" o  dono desse codigo é " + nome + " " + idade);
+            Console.WriteLine("minha idade é " + idade);
+           
+            
+            Console.WriteLine("meu nome é " + nome + " e tenho " + idade + " anos");
+          
+            Console.WriteLine($"ola meu nome é {nome}  e tenho {idade} anos");
+;
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey(); //trava da pagina para leitura
+
+        }
+
+
+
+
+
+    }
+
+}
